f00aae1ddf569ff4bd686e6e3446d755781725d4
Keks <keks@htmlacademy.ru>
2023-04-06 00:26:16 +0300

    :heavy_check_mark: Сборка #7

--- 7/js/user-form.js
+++ 7/js/user-form.js
@@ -1,84 +1,59 @@
 import { isEscapeKey, removeEventListener } from './util.js';
+import { pristine, unblockSubmitButton } from './validation.js';
 
-const MAX_NUMBER_HASHTAGS = 5;
-const HASHTAGS_GUIDE = /^#[a-zа-яё0-9]{1,19}$/i;
-const TAGS_ERROR_TEXT = 'Неправильно прописаны хештеги';
+const formElement = document.querySelector('.img-upload__form');
+const uploadFieldElement = document.querySelector('#upload-file');
+const overlayFormElement = document.querySelector('.img-upload__overlay');
+const hashtagFieldElement = overlayFormElement.querySelector('.text__hashtags');
+const commentFieldElement = overlayFormElement.querySelector('.text__description');
+const cancelButtonElement = document.querySelector('#upload-cancel');
 
-const body = document.querySelector('body');
-const formField = document.querySelector('.img-upload__form');
-const uploadField = document.querySelector('#upload-file');
-const overlayForm = document.querySelector('.img-upload__overlay');
-const hashtagField = overlayForm.querySelector('.text__hashtags');
-const commentField = overlayForm.querySelector('.text__description');
-const cancelUploadButton = overlayForm.querySelector('.img-upload__cancel');
+const isFieldFocused = () =>
+  document.activeElement === hashtagFieldElement ||
+  document.activeElement === commentFieldElement;
 
-const pristine = new Pristine(formField, {
-  classTo: 'img-upload__field-wrapper',
-  errorTextParent: 'img-upload__field-wrapper',
-  errorTextClass: 'img-upload__error'
-});
-
-const FieldFocused = () =>
-  document.activeElement === hashtagField ||
-  document.activeElement === commentField;
-
-const isValidTag = (tag) => HASHTAGS_GUIDE.test(tag);
-
-const hasValidCount = (tags) => tags.length <= MAX_NUMBER_HASHTAGS;
-
-const hasUniqueTags = (tags) => {
-  const lowerCaseTags = tags.map((tag) => tag.toLowerCase());
-  return lowerCaseTags.length === new Set(lowerCaseTags).size;
+const onShowForm = () => {
+  overlayFormElement.classList.remove('hidden');
+  document.body.classList.add('modal-open');
 };
-
-const validateTags = (value) => {
-  const tags = value
-    .trim()
-    .split(' ')
-    .filter((tag) => tag.trim().length);
-  return hasValidCount(tags) && hasUniqueTags(tags) && tags.every(isValidTag);
-};
-
-pristine.addValidator(
-  hashtagField,
-  validateTags,
-  TAGS_ERROR_TEXT
-);
 
 const onCloseForm = () => {
-  formField.reset();
-  pristine.reset();
-  overlayForm.classList.add('hidden');
-  body.classList.remove('modal-open');
+  overlayFormElement.classList.add('hidden');
+  document.body.classList.remove('modal-open');
 };
 
-cancelUploadButton.addEventListener('click', () => {
+const hideForm = () => {
   onCloseForm();
-});
+  formElement.reset();
+  pristine.reset();
+  unblockSubmitButton();
+};
 
-const closeFormByEscape = (evt) => {
-  if (isEscapeKey(evt) && !FieldFocused()) {
-    evt.preventDefault();
-    onCloseForm();
-    removeEventListener(document);
+const onPopupForm = (evt) => {
+  switch (evt.type) {
+    case 'click':
+      hideForm();
+      removeEventListener(cancelButtonElement, 'click', onPopupForm);
+      removeEventListener(document, 'keydown', onPopupForm);
+      break;
+    case 'keydown':
+      if (isEscapeKey(evt) && !isFieldFocused()) {
+        evt.preventDefault();
+        hideForm();
+        removeEventListener(document, 'keydown', onPopupForm);
+        removeEventListener(cancelButtonElement, 'click', onPopupForm);
+      }
+      break;
+    default:
+      hideForm();
+      removeEventListener(document, 'keydown', onPopupForm);
+      removeEventListener(cancelButtonElement, 'click', onPopupForm);
+      break;
   }
 };
 
-const onSubmitForm = (evt) => {
-  evt.preventDefault();
-  pristine.validate();
-};
-
-formField.addEventListener('submit', () => {
-  onSubmitForm();
-});
-
-const onShowForm = () => {
-  overlayForm.classList.remove('hidden');
-  body.classList.add('modal-open');
-  document.addEventListener('keydown', closeFormByEscape);
-};
-
-uploadField.addEventListener('change', () => {
+uploadFieldElement.addEventListener('change', () => {
   onShowForm();
+  cancelButtonElement.addEventListener('click', onPopupForm);
+  document.addEventListener('keydown', onPopupForm);
 });
